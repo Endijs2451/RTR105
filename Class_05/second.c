4ae11da31fd124a547c937eb441021e4543c169c
Endijs2451 <endijsgraudins@gmail.com>
2023-09-26 10:36:54 +0300

    Class_05

---
+++ Class_05/second.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// vienas rindas komentārs
+
+/* komentāra bloka sākums
+...
+...
+komentāra bloka beigas */
+
+int main()
+ {
+ printf("Hello World!\n"); // \n - kursora pārcelšana jaunā rindā - anaoģija ar echo -e "Hello world"
+ printf("\a"); // \a - izvada skaņu 
+ return 0;
+ }
